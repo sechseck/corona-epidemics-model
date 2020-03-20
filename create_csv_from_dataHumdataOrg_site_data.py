a64b25bb9fb6c8405c9c8424ed351f82f6fee2c6
sechseck <ralf_seschek@gmx.de>
2020-03-20 15:00:17 +0100

    uppercased all country names

--- create_csv_from_dataHumdataOrg_site_data.py
+++ create_csv_from_dataHumdataOrg_site_data.py
@@ -6,5 +6,6 @@ from pandas import DataFrame,read_csv
 if __name__ == "__main__":
 
     df = read_csv(r'./covid-19-historical-cases-by-country.csv')
+    df['ADM0_NAME']=df['ADM0_NAME'].str.upper()
     df[['ADM0_NAME','DateOfDataEntry','cum_conf']].rename(columns={'ADM0_NAME':'country','DateOfDataEntry':'date','cum_conf':'cases'}).sort_values(by=['country','date']).\
         to_csv(r'./humdata_case_numbers.csv',index=False,sep=';')
